a0516c668fbcea0eb507092fc125b313005f9e21
yelson2018 <39772132+yelson2018@users.noreply.github.com>
2019-11-25 14:01:26 -0600

    Add files via upload

--- analizador_lexico.cpp
+++ analizador_lexico.cpp
@@ -10,35 +10,14 @@
 	
 	}
 	
-	void agregar(char *&a ,char c){
-		char *a,*b;
-		int i=0;
-	
-			a[strlen(a)]=c;
-			
-			b=(char *)calloc(strlen(a),sizeof(char));
-			
-			for(i=0;i<strlen(a);i++){
-				b[i]=a[i];
-				
-			}
-			
-			free(a);	
-			a=(char *)calloc(strlen(b)+1,sizeof(char));
-			
-			for(i=0;i<strlen(b);i++)a[i]=b[i];
-			
-			
-				for(i=0;i<strlen(a)-1;i++)printf("%c",a[i]);
-					
-		
-	}
+
 	
 	void leerCxC(){
 			int finTexto=0;
 			int iCaracter;
 			
 		char *a,*b;
+		int i=0,x=0;
 		
 		a=(char *)calloc(1,sizeof(char));
 			
@@ -46,11 +25,39 @@
 			char* nArchivo="programa.txt";
 			archivo=fopen(nArchivo,"r");
 	
-		
+																													
 			while(!feof(archivo)){
 					iCaracter=fgetc(archivo);
-				agregar(a,iCaracter);
+					
+			
+					if(iCaracter=='\n' ){
+					
+					}else{
+							
+					a[x]=iCaracter;
+					
+					b=(char *)calloc(x+1,sizeof(char));
+					
+					for(i=0;i<x+1;i++)b[i]=a[i];
+					
+					free(a);
+						
+					a=(char *)calloc(x+2,sizeof(char));
+					
+					for(i=0;i<x+1;i++)a[i]=b[i];
+							
+					for(i=0;i<x+1;i++){
+					
+						printf("%c ",a[i]);
+					}
+					
+				
+					x=+1;
+					
+				}
 			}
+			
+		
 			
 				
 		
